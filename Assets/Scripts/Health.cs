18c40536274bfda90b736a23ac71e18e31f4852e
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2022-06-17 01:42:14 +0200

    Added 2D Canvas
    - 2D Canvas that tracks the 3D canvas
    - Health bar works properly now

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField]
+    private Slider playerSlider3D;
+    private Slider playerSlider2D;
+
+    Stats statsScript;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        statsScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Stats>();
+        playerSlider3D = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Slider>();
+
+        playerSlider2D = GetComponent<Slider>();
+
+        playerSlider3D.maxValue = statsScript.maxHealth;
+        playerSlider2D.maxValue = statsScript.maxHealth;
+        statsScript.health = statsScript.maxHealth;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerSlider2D.value = statsScript.health;
+        playerSlider3D.value = playerSlider2D.value;
     }
 }
