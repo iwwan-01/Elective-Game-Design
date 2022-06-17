9a15a6ade601d4f4482f378b68051151be801e3c
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2022-06-17 10:56:37 +0200

    Win Condition

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    CoinCounter coinCounter;
+    KillCounter killCounter;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+       coinCounter = GameObject.Find("Coin Counter Canvas 2D").GetComponentInChildren<CoinCounter>();
+       killCounter = GameObject.Find("Kill Counter Canvas 2D").GetComponentInChildren<KillCounter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (killCounter.killAmount == 8 && coinCounter.coinAmount == 8)
+        {
+            SceneManager.LoadScene("Win");
+        } 
+    }
+}
